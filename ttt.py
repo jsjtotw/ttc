799af407174185c6010255b2e76c3243ffca0f94
jsjtotw <134265805+jsjtotw@users.noreply.github.com>
2024-02-03 20:01:48 +0800

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -3,27 +3,14 @@ arr = np.char.array([["-","-","-"],["-","-","-"],["-","-","-"]])
 print(arr)
 def choi(ele):
   cho = int(input("Choose which square to choose (1-9)"))
-  if cho == 1:
-    arr[0][0] = ele
-  elif cho == 2:
-    arr[0][1] = ele
-  elif cho == 3:
-    arr[0][2] = ele
-  elif cho == 4:
-    arr[1][0] = ele
-  elif cho == 5:
-    arr[1][1] = ele
-  elif cho == 6:
-    arr[1][2] = ele
-  elif cho == 7:
-    arr[2][0] = ele
-  elif cho == 8:
-    arr[2][1] = ele
-  elif cho == 9:
-    arr[2][2] = ele
+  if 1 <= cho <= 9:
+    row = (cho - 1) // 3
+    col = (cho - 1) % 3
+    arr[row][col] = ele
   else:
-    print("Put an input from 0-9")
+    print("Please choose a number between 1 and 9.")
+  if ele
   print(arr)
-while victory := false:
+while True:
   choi(ele="X")
   choi(ele="O")
